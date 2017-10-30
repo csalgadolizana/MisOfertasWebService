0c59e9d1ace6157dc20ee6eed1d2685b2e281abd
csalgadolizana <csalgadolizana@gmail.com>
2017-10-30 17:03:42 -0300

    update

--- src/java/Servicios/OfertaVistaService.java
+++ src/java/Servicios/OfertaVistaService.java
@@ -5,7 +5,7 @@
  */
 package Servicios;
 
-import Entidades.OfertasVisitas;
+import Entidades.Ofertavisi;
 import java.util.Date;
 import java.util.List;
 import javax.jws.WebService;
@@ -28,15 +28,16 @@ public class OfertaVistaService {
     EntityManager em;
 
     @WebMethod(operationName = "Listado_oferta_visitas")
-    public List<OfertasVisitas> ListadoOfertaVisitas() {
-        TypedQuery<OfertasVisitas> query = em.createNamedQuery("OfertasVisitas.findAll", OfertasVisitas.class);
+    public List<Ofertavisi> ListadoOfertaVisitas() {
+        TypedQuery<Ofertavisi> query = em.createNamedQuery("Ofertavisi.findAll", Ofertavisi.class);
         return query.getResultList();
     }
 
     @WebMethod(operationName = "Crear_oferta_visitas")
     public String CrearOfertaVisitas(
-            @WebParam(name = "fecha_vista") Date fecha_vista, @WebParam(name = "id_oferta_visita") int id_oferta_visita,
-            @WebParam(name = "id_cliente") int id_cliente, @WebParam(name = "id_oferta") int id_oferta
+            @WebParam(name = "fecha_vista") Date fecha_vista,
+            @WebParam(name = "id_cliente") int id_cliente,
+            @WebParam(name = "id_oferta") int id_oferta
     ) {
         try {
             StoredProcedureQuery query = em.createStoredProcedureQuery("CREAR_OFERTA_VISITA");
@@ -46,7 +47,7 @@ public class OfertaVistaService {
             query.registerStoredProcedureParameter("ID_OFE", Number.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("SALIDA", Number.class, ParameterMode.OUT);
             query.setParameter("FECHV", fecha_vista);
-            query.setParameter("IDOFV", id_oferta_visita);
+            query.setParameter("IDOFV", 40);
             query.setParameter("ID_CLI", id_cliente);
             query.setParameter("ID_OFE", id_oferta);
             query.execute();
@@ -55,7 +56,7 @@ public class OfertaVistaService {
             return 0 + "";
         }
     }
-    
+
     @WebMethod(operationName = "Eliminar_oferta_visitas")
     public String EliminarOfertaVisita(@WebParam(name = "id") int idd) {
         try {
